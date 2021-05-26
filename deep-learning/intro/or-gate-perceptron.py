837c0a79e9448f52afa2cc52b2e26fd7c32d5bfa
tom <tomjohnsonellis@protonmail.com>
2021-05-26 11:39:39 +0100

    Add comments and randomise bias

--- deep-learning/intro/or-gate-perceptron.py
+++ deep-learning/intro/or-gate-perceptron.py
@@ -10,12 +10,14 @@ All you need is some matrix multiplication!
 import random
 
 learning_rate = 0.1
-bias = 1
+bias = random.random()
 # 3 random weights
 weights = [random.random(), random.random(), random.random()]
+print(f"Starting weights: {weights}")
 
 
 def simple_perceptron(input_one, input_two, output):
+    # See what the result of the perceptron is
     perceptron_output = input_one * weights[0] + input_two * weights[1] + bias * weights[2]
 
     # This is a very simple activation function, called "Heaviside"
@@ -24,14 +26,19 @@ def simple_perceptron(input_one, input_two, output):
     else:
         perceptron_output = 0
 
+    # If our perceptron is incorrect, error will be 1 so a change will happen
+    # If our perceptron is correct, error will be 0 so nothing happens
     error = output - perceptron_output
-    weights[0] += error * input_one * learning_rate
-    weights[1] += error * input_two * learning_rate
-    weights[2] += error * bias * learning_rate
-    print(f"Weights updated to: {weights}")
+    # Update each weight as necessary
+    if error:
+        weights[0] += error * input_one * learning_rate
+        weights[1] += error * input_two * learning_rate
+        weights[2] += error * bias * learning_rate
+        print(f"Weights updated to: {weights}")
 
 
-for i in range(10):
+# Let's "train" the perceptron!
+for i in range(1000):
     simple_perceptron(1, 1, 1)
     simple_perceptron(1, 0, 1)
     simple_perceptron(0, 1, 1)
@@ -39,7 +46,6 @@ for i in range(10):
 
 
 # Test cases
-
 def test(x, y):
     perceptron_output = x * weights[0] + y * weights[1] + bias * weights[2]
     if perceptron_output > 0:

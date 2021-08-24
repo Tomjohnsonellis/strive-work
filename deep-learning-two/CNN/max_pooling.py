ea79c54896a34d0b0959cc84604b51e8776f3cb2
tom <tomjohnsonellis@protonmail.com>
2021-08-24 15:01:36 +0100

    Finish pixellate function Refactor code

--- deep-learning-two/CNN/max_pooling.py
+++ deep-learning-two/CNN/max_pooling.py
@@ -1,34 +1,37 @@
 import torch
 import torchvision
 import matplotlib.pyplot as plt
-import numpy as np
 from torchvision.io.image import ImageReadMode
 
-# import os
-# print(os.getcwd())
-
-
-def max_pool_image(image_tensor) -> None:
+def max_pool_image(image_tensor, kernel_size=50, stride=50) -> None:
+    # Convert the tensor to a float so that we can pool it
     image_tensor = image_tensor.float()
-    # m = torch.nn.AvgPool2d(1,stride=1)
-    # pooled = m(image_tensor)
-    # display_image(pooled)
+    # We'll use average pooling for this
+    m = torch.nn.AvgPool2d(kernel_size=kernel_size,stride=stride)
+    pooled_image = m(image_tensor)
 
-    m = torch.nn.AvgPool2d(kernel_size=3,stride=2)
-    r_pool = image_tensor[0]
-    r_pool = m(r_pool)
-    # g_pool = torch.Tensor(m(image_tensor[1]))
-    # b_pool = torch.Tensor(m(image_tensor[2]))
-    # seperate_pools = torch.empty((3,r_pool.shape[0],r_pool.shape[1]))
-    # seperate_pools[0] = r_pool
-    # seperate_pools[1] = g_pool
-    # seperate_pools[2] = b_pool
-    # display_image(r_pool)
+    print(pooled_image.shape)
+
+    # Debugging info: I was having trouble getting the entire image to display properly/ at all
+    # print(pooled_image[0].mean())
+    # print(pooled_image[1].mean())
+    # print(pooled_image[2].mean())
+    # print(pooled_image[0].shape)
+
+    # pooled_image = pooled_image.permute(1,2,0)
+    # print("permuting")
+    # print(pooled_image.shape)
     
-    return
+    # All my values are floats, from the conversion before
+    # for x in pooled_image:
+    #     print(x)
+
+    # imshow() requires ints, so convert the tensor to int
+    pooled_image = pooled_image.int()
+    return pooled_image
 
     
-def display_image(image_tensor) -> None:
+def display_image(image_tensor:torch.Tensor()) -> None:
     # Tensors are in the form: channel, height, width
     # This will change it to: height, width, channel
     try:
@@ -37,56 +40,80 @@ def display_image(image_tensor) -> None:
         plottable_image = image_tensor
         pass
     print(plottable_image.shape)
+
     plt.imshow(plottable_image)
     plt.show()
     return
 
-# def 
+def pixellate(jpg_or_png_path:str, kernel_size:int=25, stride:int=25) -> None:
+    image = torchvision.io.read_image(jpg_or_png_path, ImageReadMode.RGB)
+    image = image.float()
+    display_image(max_pool_image(image, kernel_size=kernel_size, stride=stride))
+    return
 
 
-image = torchvision.io.read_image("deep-learning-two/CNN/fruits2.jpg", ImageReadMode.RGB)
-image = image.float()
-# display_image(fruit)
-print(f"Image's shape: {image.shape}")
 
-red = image[0]
-green = image[1]
-blue = image[2]
-p = torch.nn.MaxPool2d(10,10)
-red = red.unsqueeze(2)
-red = red.permute(2,0,1)
-block_red = p(red)
+if __name__ == '__main__':
+   
+    pixellate("deep-learning-two/CNN/fruits2.jpg")
 
-q = torch.nn.AvgPool2d(10,10)
-green = green.unsqueeze(2)
-green = green.permute(2,0,1)
-block_green = q(green)
-blue = blue.unsqueeze(2)
-blue = blue.permute(2,0,1)
-block_blue = q(blue)
 
-print("="*50)
-x = block_red.shape[1]
-y = block_red.shape[2]
-print(block_red.shape)
+    # Old code, it has now been refactored into pixellate()
+    # image = torchvision.io.read_image("deep-learning-two/CNN/fruits.jpg", ImageReadMode.RGB)
+    # image = image.float()
+    # # display_image(fruit)
+    # print(f"Image's shape: {image.shape}")
+    # pixellated_image = max_pool_image(image, 50, 50)
+    # display_image(pixellated_image)
 
-blocky = torch.empty([3,x,y])
-blocky[0] = block_red
-blocky[1] = block_green
-blocky[2] = block_blue
-# print(big_red_blocks.size)
 
-# image[0] = block_red
-# image[1] = block_green
-# image[2] = block_blue
-test = block_green
-print(test)
-try:
-    plottable_image = test.permute(1,2,0)
-except:
-    plottable_image = test
-plt.imshow(plottable_image)
-plt.show()
+
+"""
+Below is a lot of ugly debugging code, left in to remind myself to:
+1) Use a different function or file to figure out what the problem is rather than create a huge mess where I'm working
+2) Take a break and consider different approaches if getting something working is posing an issue
+"""
+
+
+
+# red = image[0]
+# green = image[1]
+# blue = image[2]
+# p = torch.nn.MaxPool2d(10,10)
+# red = red.unsqueeze(2)
+# red = red.permute(2,0,1)
+# block_red = p(red)
+
+# q = torch.nn.AvgPool2d(10,10)
+# green = green.unsqueeze(2)
+# green = green.permute(2,0,1)
+# block_green = q(green)
+# blue = blue.unsqueeze(2)
+# blue = blue.permute(2,0,1)
+# block_blue = q(blue)
+
+# print("="*50)
+# x = block_red.shape[1]
+# y = block_red.shape[2]
+# print(block_red.shape)
+
+# blocky = torch.empty([3,x,y])
+# blocky[0] = block_red
+# blocky[1] = block_green
+# blocky[2] = block_blue
+# # print(big_red_blocks.size)
+
+# # image[0] = block_red
+# # image[1] = block_green
+# # image[2] = block_blue
+# test = block_green
+# print(test)
+# try:
+#     plottable_image = test.permute(1,2,0)
+# except:
+#     plottable_image = test
+# plt.imshow(plottable_image)
+# plt.show()
 
 
 

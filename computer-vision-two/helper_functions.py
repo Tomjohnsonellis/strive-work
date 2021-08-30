841e52e18ba35155dc47dfab67acd790defa750d
tom <tomjohnsonellis@protonmail.com>
2021-08-30 11:00:45 +0100

    Upload QR reader work

--- computer-vision-two/helper_functions.py
+++ computer-vision-two/helper_functions.py
@@ -1,8 +1,18 @@
 import cv2
 
 
-def display_image(image_array, image_name="My Image"):
+def display_image(image_array, image_name="My Image") -> None:
     cv2.imshow(image_name, image_array)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cv2.waitKey(1)
+    return
+
+def annotate_quadrangle(image_array, points_for_quadrangle, thickness=5):
+    thickness = thickness
+    image_with_quadrangle = image_array.copy()
+    cv2.line(image_with_quadrangle, points_for_quadrangle[0], points_for_quadrangle[1], (0,0,255), thickness)
+    cv2.line(image_with_quadrangle, points_for_quadrangle[1], points_for_quadrangle[2], (255,0,0), thickness)
+    cv2.line(image_with_quadrangle, points_for_quadrangle[2], points_for_quadrangle[3], (0,255,0), thickness)
+    cv2.line(image_with_quadrangle, points_for_quadrangle[3], points_for_quadrangle[0], (255,128,255), thickness)
+    return image_with_quadrangle
---
+++ computer-vision-two/qr_reader.py
@@ -0,0 +1,49 @@
+from helper_functions import display_image, annotate_quadrangle
+import cv2
+from cv2 import QRCodeDetector
+
+print(cv2.__version__)
+# First thing to do is load an image containing a qr code
+image_with_qr = cv2.imread('computer-vision-two/assets/qr.jpg')
+# This image is a bit big so let's resize it
+image_with_qr = cv2.resize(image_with_qr, (950, 600))
+
+
+# Then we can try to detect it, this returns a "True/False" 
+# and coordinates that make a square containing the QR code
+data_found, points = QRCodeDetector().detect(image_with_qr)
+if data_found:
+    print("QR Found!")
+else:
+    print("No QR detected, exiting...")
+    exit()
+print("="*50)
+# points is actually a 1x4x2 array, we just need the 4x2 info
+#print(points.shape)
+points = points[0].astype(int)
+print(points)
+# We can use the points to draw lines
+# thickness = 5
+# cv2.line(image_with_qr, points[0], points[1], (0,0,255), thickness)
+# cv2.line(image_with_qr, points[1], points[2], (255,0,0), thickness)
+# cv2.line(image_with_qr, points[2], points[3], (0,255,0), thickness)
+# cv2.line(image_with_qr, points[3], points[0], (255,128,255), thickness)
+
+QR_box = annotate_quadrangle(image_with_qr, points)
+
+display_image(QR_box)
+
+# Now for decoding the QR as well
+qr_found, points, corrected_qr = QRCodeDetector().detectAndDecode(image_with_qr)
+if qr_found:
+    print("QR Detected!")
+    print(f"It says: {qr_found}")
+    print("Saving and displaying detected code...")
+    cv2.imwrite("computer-vision-two/assets/corrected_qr.jpg", corrected_qr)
+    display_image(corrected_qr)
+else:
+    print("No QR detected, exiting...")
+    exit()
+
+
+

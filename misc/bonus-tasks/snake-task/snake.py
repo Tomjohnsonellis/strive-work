ba5dc8c46a4b62bfe0ce8d0ff717c240b83679d1
tom <tomjohnsonellis@protonmail.com>
2021-09-19 15:43:52 +0100

    Add location based generation

--- misc/bonus-tasks/snake-task/snake.py
+++ misc/bonus-tasks/snake-task/snake.py
@@ -40,14 +40,21 @@ def validate_board(board_array:list[int]) -> bool:
 # 0 1 2 3 4
 # 1 
 # 2
-board = [3,5]
+board = [4,5]
 print(f"Board valid?: {validate_board(board)}")
 
-def generate_snake(snake_length:int=4, board_array:list[int,int]=board) -> list[list[int,int]]:
+def generate_snake(snake_length:int=10, board_array:list[int,int]=board) -> list[list[int,int]]:
     y = random.randint(0, board_array[0])
     x = random.randint(0, board_array[1])
     snake_head = [y,x]
+
+    # TESTING
+    snake_head = [3,4]
+
     snake = [snake_head]
+    # print(snake_head)
+    create_snake_body(snake, snake_length)
+
 
     # Create X blocks
     # Use the length and just create all the blocks at once
@@ -56,16 +63,139 @@ def generate_snake(snake_length:int=4, board_array:list[int,int]=board) -> list[
 
     return snake
 
-def create_snake_body(snake:list[list[int,int]], board_array:list[int,int]=board) -> list[list[int,int]]:
+def create_snake_body(snake:list[list[int,int]], snake_length:int, board_array:list[int,int]=board) -> list[list[int,int]]:
+    pieces_to_generate = snake_length - 1
+    
     # The idea here is to build the snake in a way that avoids the snake extending itself into a corner
     mid_y = board_array[0]/2
     mid_x = board_array[1]/2
+    
+
+
     # We'll split the board into 4 sections, and the section that the snake head is in determines how it will be extended
-    head_location = ""
-    if snake[0][0] < 
+    pos_top = False
+    pos_left = False
+    if snake[0][0] <= mid_y:
+        pos_top = True
+    else:
+        pos_top = False
+    
+    if snake[0][1] <= mid_x:
+        pos_left = True
+    else:
+        pos_left = False
 
+    # TESTING
+    print(f"MID POINT: {mid_y, mid_x}")
+    print(f"SNAKE HEAD: {snake[0]}")
+    print(f"TOP: {pos_top}")
+    print(f"LEFT: {pos_left}")
 
+    generation_point = snake[0]
+    # If the snake is in the top section, generate sections downwards until the edge of the board.
+    if pos_top:
+        distance_to_wall = board_array[0] - generation_point[0] - 1
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0] + space + 1, generation_point[1]])
+            print("Added chunk...")
+            print(snake)
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    # Or if it's in the bottom section, generate upwards
+    else:
+        distance_to_wall = 0 + generation_point[0]
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0] - space - 1, generation_point[1]])
+            print("Added chunk...")
+            print(snake)
+            
+            if pieces_to_generate == 0:
+                break
+            pieces_to_generate -= 1
 
+    # Snake length is variable, so return if we're done
+    if pieces_to_generate == 0:
+                return snake
+
+    # Otherwise, continue generation from the new tail
+    generation_point = snake[-1]
+    # If we're in the left section, generate right
+    if pos_left:
+        distance_to_wall = board_array[1] - generation_point[1] - 1
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0], generation_point[1] + space + 1])
+            print("Added chunk...")
+            print(snake)
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    # Or if we're in the right section, generate left
+    else:
+        distance_to_wall = 0 + generation_point[1]
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0], generation_point[1] - space - 1])
+            print("Added chunk...")
+            print(snake)
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    
+    # Snake length is variable, so return if we're done
+    if pieces_to_generate == 0:
+                return snake
+    # In most cases, going vertical and horizontal should be enough to generate the snake.
+    # But for edge cases, we'll repeat the process, from here the snake would have generated into a corner.
+    # This is a programming sin but it's a few hours in and I'm still generating the snake so below is the same code.
+    generation_point = snake[-1]
+    if generation_point[0] <= mid_y:
+        pos_top = True
+    else:
+        pos_top = False
+    
+    if generation_point[1] <= mid_x:
+        pos_left = True
+    else:
+        pos_left = False
+    if pos_top:
+        distance_to_wall = board_array[0] - generation_point[0] - 1
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0] + space + 1, generation_point[1]])
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    else:
+        distance_to_wall = 0 + generation_point[0]
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0] - space - 1, generation_point[1]])
+            print("Added chunk...")
+            print(snake)
+            
+            if pieces_to_generate == 0:
+                break
+            pieces_to_generate -= 1
+
+    if pieces_to_generate == 0:
+                return snake
+
+    generation_point = snake[-1]
+    if pos_left:
+        distance_to_wall = board_array[1] - generation_point[1] - 1
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0], generation_point[1] + space + 1])
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    else:
+        distance_to_wall = 0 + generation_point[1]
+        for space in range(distance_to_wall):
+            snake.append([generation_point[0], generation_point[1] - space - 1])
+            pieces_to_generate -= 1
+            if pieces_to_generate == 0:
+                break
+    
+
+    print(snake)
 
     return snake
 

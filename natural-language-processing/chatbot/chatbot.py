8f59e2cb1dd27f13978d74fd950e5cdfcc6c8560
tom <tomjohnsonellis@protonmail.com>
2021-09-24 12:40:29 +0100

    Upload ascii banner

---
+++ natural-language-processing/chatbot/chatbot.py
@@ -0,0 +1,26 @@
+"""
+Goal of today is to make a simple chatbot therapist.
+
+"""
+
+
+import spacy
+import numpy as np
+import json
+
+
+
+def print_intro() -> None:
+
+    with open("natural-language-processing/chatbot/logo.txt") as f:
+        print("".join([line for line in f]))
+    print()
+    print("-"*100)
+    return None
+
+
+
+
+
+if __name__ == "__main__":
+    pass

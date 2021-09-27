1c5c32c0821582df287c0932443d70f260276345
tom <tomjohnsonellis@protonmail.com>
2021-09-27 15:41:14 +0100

    Upload WORKING poke/food classifier

--- natural-language-processing/named-entity-recognition/pokemon.py
+++ natural-language-processing/named-entity-recognition/pokemon.py
@@ -59,19 +59,123 @@ def generate_sentences(entities, extra_data=300):
         # Create a sentence
         text = base_sentences[i].replace("x",entity)
         # Store the needed info
-        location_info = (starting_index, ending_index, "POKEMON")
+        pos = (starting_index, ending_index, "POKEMON")
+        # This looks a bit odd, but we want to have a list of all entities, in this case it will always be one element,
+        # But for more complex sentences we may want more than one
+        locs = []
+        locs.append(pos)
         # Store it all in our training dataset
-        training_sentences.append( (text, {"entities":location_info}))
+        element = (text, {"entities":locs})
+        training_sentences.append(element)
 
     # Extension: Create more complex sentences
 
 
     return training_sentences
 
+# Now we have a dataset to work with.
+# We can now begin update a model.
+def train_model(nlp_model, training_data, epochs=10, save=False):
+    # Load a model then pass it here for updating
+    # Add our new tag
+    pokemon_ner = nlp_model.get_pipe("ner")
+    pokemon_ner.add_label("FOOD")
+    pokemon_ner.add_label("POKEMON")
+
+    # Disable everything we don't need for fast training
+    things_to_affect = ["ner", "trf_wordpiecer","trf_tok2vec"]
+    unaffected = [pipe for pipe in nlp_model.pipe_names if pipe not in things_to_affect]
+    # Using only the things we need...
+    with nlp_model.disable_pipes(*unaffected):
+        for iteration in range(epochs):
+            # Shuffle the data, to help prevent overfitting
+            random.shuffle(training_data)
+            losses = {}
+
+            # Batch up the data
+            batches = minibatch(training_data, size=32)
+            # Work through each batch
+            for index, batch in enumerate(batches):
+                for text, entities in batch:
+                    # Make a doc out of the text
+                    doc = nlp_model.make_doc(text)
+                    # Create a spaCy "Example", which is two Docs, one is the correctly labelled info, the other is the pipeline's predictions
+                    example = Example.from_dict(doc, entities)
+                    # Update our nlp model with the info
+                    nlp_model.update([example], losses=losses, drop=0)
+                print(f"Iteration: {iteration} | Batch: {index} | Loss: {losses}")
+        
+    if save:
+        nlp_model.to_disk("natural-language-processing/named-entity-recognition/data/poke.nlp")
+        print("Saved!")
+    
+    return nlp_model
 
 
+def test_model(model):
+    test_one = model("My charizard loves soup")
+    test_two = model("Don't give pasta to rattata")
+    test_three = model("I gave slowbro a cappuccino")
+    tests = [test_one, test_two, test_three]
+    for doc in tests:
+        print("Entities: ", [(entity.text, entity.label_) for entity in doc.ents])
+    return
+
+
+def get_food_training():
+    words = ["ketchup", "pasta", "carrot", "pizza",
+         "garlic", "tomato sauce", "basil", "carbonara",
+         "eggs", "cheek fat", "pancakes", "parmigiana", "eggplant",
+         "fettucine", "heavy cream", "polenta", "risotto", "espresso",
+         "arrosticini", "spaghetti", "fiorentina steak", "pecorino",
+         "maccherone", "nutella", "amaro", "pistachio", "coca-cola",
+         "wine", "pastiera", "watermelon", "cappuccino", "ice cream",
+         "soup", "lemon", "chocolate", "pineapple"]
+
+    with open("natural-language-processing/named-entity-recognition/data/food.txt") as foodfile:
+       food_dataset = foodfile.readlines()
+    food_dataset = [sentence.lower() for sentence in food_dataset]
+    train_data = []
+
+    for sentence in food_dataset:
+        entities = []
+        for word in words:
+            if word in sentence:
+                # Find their indexes, the character positions of each word. 
+                # "red" in "the red cat" would be (4,7)
+                start_index = sentence.index(word)
+                end_index = len(word) + start_index
+                # Also add the label while we're here
+                pos = (start_index, end_index, "FOOD")
+                # Add it to the entities list
+                entities.append(pos)
+        # Store the sentence and accompanying food locations
+        element = (sentence.rstrip("\n"), {"entities":entities})
+        # Add that to our training data
+        train_data.append(element)
+
+    return train_data
+
+
+def combine_datasets(food, pokemon):
+    big_dataset = []
+    for x in food:
+        big_dataset.append(x)
+    for x in pokemon:
+        big_dataset.append(x)
+
+    return big_dataset
 
 if __name__ == "__main__":
     pokemon = get_entities()
-    sentences = generate_sentences(pokemon)
-    print(sentences)
+    poke_sentences = generate_sentences(pokemon)
+    food_sentences = get_food_training()
+    # print(food_sentences)
+    # print("-"*50)
+    combined_data = combine_datasets(food_sentences, poke_sentences)
+    
+    food_nlp = spacy.load("natural-language-processing/named-entity-recognition/data/trained.nlp")
+    test_model(food_nlp)
+    food_and_poke = train_model(food_nlp, combined_data, save=True)
+
+    test_model(food_and_poke)
